b63abf76b0b58f828e50ce60989cc5a05d5a4e4d
willdunlop <william.dunlop.1993@gmail.com>
2017-01-31 17:53:00 +1100

    nothing much

--- client/components/button_component.js
+++ client/components/button_component.js
@@ -1,18 +1,25 @@
 //imports
 import React from 'react';
 
+//dummy data
+const _ELEV = [
+   {id: 1, topic: 'Introduction'},
+   {id: 2, topic: 'How to use the interface'},
+   {id: 3, topic: 'git gud'}
+ ]
+
 //create/export component
 export default class ElevioHelpButtonComponent extends React.Component {
   whenClicked(){
-    if (window._elev) {
-      window._elev.openArticle(/*this.data.id*/);
+    if (_ELEV) {
+      _ELEV.id(this.props.id);
     } else {
       console.log('_elev is not attached to window, help is unavailable');
     }
   }
   render(){
     return(
-      <button className="btn btn-default"  onClick={this.whenClicked}>
+      <button className="btn btn-default"  onClick={this.props.whenClicked} >
         Help?
       </button>
     );
---
+++ client/components/dummy_elev.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+//
+// const _ELEV = [
+//    {id: 1, topic: 'Introduction'},
+//    {id: 2, topic: 'How to use the interface'},
+//    {id: 3, topic: 'git gud'}
+//  ]
+//
+// export default class DummyElev extends React.Component {
+//   const eaElev = _ELEV.map(){}
+//   return(
+//     <div>{eaElev}</div>
+//   )
+//
+//
+//   }
+// }
